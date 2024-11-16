6175470caa6c7f559949c4d0de5c58e5948a7caa
Shawn Li <shli0267@uni.sydney.edu.au>
2024-11-16 11:42:48 +1100

    handle errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,10 +1,15 @@
 "use client";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
-import SimpleMDE from "react-simplemde-editor";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 interface IssueForm {
   title: string;
@@ -12,27 +17,41 @@ interface IssueForm {
 }
 
 const NewIssuePage = () => {
+  const [error, setError] = useState("");
+
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", data);
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root placeholder="title... " {...register("title")} />
-      <Controller
-        name="discription"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="discription..." {...field} />
-        )}
-      />
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-6">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("Unexpected issue occured!");
+            console.log(error);
+          }
+        })}
+      >
+        <TextField.Root placeholder="title... " {...register("title")} />
+        <Controller
+          name="discription"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="discription..." {...field} />
+          )}
+        />
 
-      <Button>Submit!</Button>
-    </form>
+        <Button>Submit!</Button>
+      </form>
+    </div>
   );
 };
 
