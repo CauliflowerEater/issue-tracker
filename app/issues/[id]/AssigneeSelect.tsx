b23916129d68dc9cd11f28503eccfe12969bc1a7
Shawn Li <shli0267@uni.sydney.edu.au>
2024-11-30 19:41:43 +1100

    Build the assignee select component

---
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { Select } from "@radix-ui/themes";
+
+const AssigneeSelect = () => {
+  return (
+    <Select.Root>
+      <Select.Trigger placeholder="Assign..." />
+      <Select.Content>
+        <Select.Group>
+          <Select.Label>Suggestions</Select.Label>
+          <Select.Item value="shit">Mr.Shit</Select.Item>
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default AssigneeSelect;
