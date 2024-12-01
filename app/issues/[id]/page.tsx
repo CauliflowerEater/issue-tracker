38d7e401241c437b50c22513c8a2d11cfb84ed50
Shawn Li <shli0267@uni.sydney.edu.au>
2024-12-01 17:54:49 +1100

    Assign an issue to a user

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -30,7 +30,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction="column" gap="3">
-            <AssigneeSelect />
+            <AssigneeSelect issue={issue} />
             <EditIssueButton id={issue.id} />
             <DeleteIssueButton issueId={issue.id} />
           </Flex>
