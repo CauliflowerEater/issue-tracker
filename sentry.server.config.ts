7061947c18d3dc999f06acd9b5c977e7e92d8f18
Shawn Li <shli0267@uni.sydney.edu.au>
2024-12-04 23:02:57 +1100

    Set up error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,12 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://0d8ecc25119ed85bd2a5a7af06bf76c3@o4508409694257153.ingest.us.sentry.io/4508409717915648",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
